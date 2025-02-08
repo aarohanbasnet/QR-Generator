d60030aafa6a1211ff01a607a87ebd5a02813c6e
aarohanbasnet <letsmailaarohan@gmail.com>
2025-02-08 16:02:46 +0545

    Added smooth animination

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ let  qrText = document.getElementById("qrText");
 function generateQR(){
     
 if(qrText.value.length > 0){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
-  imgBox.classList.add("show-img");
+
+    let qrURL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
+    
+    //Set the image source only after it's fully loaded preventing from UI freeze while loading
+    qrImage.onload = function(){
+        imgBox.classList.add("show-img");
+    };
+    qrImage.src = qrURL;
 }else{
     qrText.classList.add('error');
+    console.log(qrText.classList);
     setTimeout(()=>{
         qrText.classList.remove('error');
-    },1000);
+    },600);
 }
 }
